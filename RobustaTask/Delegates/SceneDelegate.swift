70e48662b1f2f53731c4e9f176aeb7e30dc827a3
nourhanboghdady <nourhan.boghdady@easwaaqmisr.com>
2023-04-07 21:20:37 +0200

    inject viewModel

--- RobustaTask/Delegates/SceneDelegate.swift
+++ RobustaTask/Delegates/SceneDelegate.swift
@@ -19,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        let controller = HomeViewController()
+        let viewModel = HomeViewModel()
+        let controller = HomeViewController(homeViewModel: viewModel)
         window?.rootViewController = controller
         window?.makeKeyAndVisible()
         
